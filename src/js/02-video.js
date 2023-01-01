4fb0de79f50a4ec7e76d19e71f84a60ec0417e0c
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-01-01 12:03:34 +0200

    updated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,3 +9,9 @@ player.on('play', function () {
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
+
+const timeupdate = function (data) {
+  // data is an object containing properties specific to that event
+};
+
+player.on('play', timeupdate);
