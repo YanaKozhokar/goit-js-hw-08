87ca6d361cc24fd0c8a73688e4d0865c97277503
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-01-05 22:13:10 +0200

    updated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,21 +6,6 @@ const onTimeUpdate = function (event) {
   localStorage.setItem('videoplayer-current-time', event.seconds);
 };
 
-player
-  .setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
 player.on('timeupdate', onTimeUpdate);
