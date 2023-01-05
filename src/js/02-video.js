d7dba2736414df75cc88db5cab148521b051dd34
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-01-05 11:44:51 +0200

    task-02 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,16 +2,25 @@ import Vimeo from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+player.on('pause', timeupdate);
 
 const timeupdate = function (data) {
-  // data is an object containing properties specific to that event
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 };
 
-player.on('play', timeupdate);
+player
+  .setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
