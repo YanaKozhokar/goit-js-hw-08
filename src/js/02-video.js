4f67ebac592f2042d5eb67a20d3c9d2de6e5c85b
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-01-06 12:25:47 +0200

    updated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,34 +1,3 @@
-// import throttle from 'lodash.throttle';
-// import Vimeo from '@vimeo/player';
-
-// const STORAGE_KEY = 'videoplayer-current-time';
-
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo(iframe);
-
-// player.on('timeupdate', throttle(onVideoPlayer, 1000));
-
-// function onVideoPlayer(event) {
-//   let dataString = event.seconds;
-//   localStorage.setItem(STORAGE_KEY, dataString);
-// }
-
-// const savedSeconds = localStorage.getItem(STORAGE_KEY);
-
-// if (savedSeconds) {
-//   player
-//     .setCurrentTime(savedSeconds)
-//     .then(function (seconds) {})
-//     .catch(function (error) {
-//       switch (error.name) {
-//         case 'RangeError':
-//           break;
-//         default:
-//           break;
-//       }
-//     });
-// }
-
 import Vimeo from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
