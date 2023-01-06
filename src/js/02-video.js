652177acef042eb638826d5c97796fb7b49e138f
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-01-06 19:52:51 +0200

    updated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,14 +2,12 @@ import Vimeo from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
-if (localStorage.getItem('videoplayer-current-time') === undefined) {
-  return;
-} else {
-  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-}
-
 const onTimeUpdate = function (event) {
   localStorage.setItem('videoplayer-current-time', event.seconds);
 };
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
 
 player.on('timeupdate', onTimeUpdate);
