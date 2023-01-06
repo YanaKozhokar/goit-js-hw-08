5a2911eb095729bcc9128958b6df3df7782a208e
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-01-06 12:15:18 +0200

    updated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,42 +1,46 @@
-import throttle from 'lodash.throttle';
-import Vimeo from '@vimeo/player';
-
-const STORAGE_KEY = 'videoplayer-current-time';
-
-const iframe = document.querySelector('iframe');
-const player = new Vimeo(iframe);
-
-player.on('timeupdate', throttle(onVideoPlayer, 1000));
-
-function onVideoPlayer(event) {
-  let dataString = event.seconds;
-  localStorage.setItem(STORAGE_KEY, dataString);
-}
-
-const savedSeconds = localStorage.getItem(STORAGE_KEY);
-
-if (savedSeconds) {
-  player
-    .setCurrentTime(savedSeconds)
-    .then(function (seconds) {})
-    .catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          break;
-        default:
-          break;
-      }
-    });
-}
-
+// import throttle from 'lodash.throttle';
 // import Vimeo from '@vimeo/player';
+
+// const STORAGE_KEY = 'videoplayer-current-time';
+
 // const iframe = document.querySelector('iframe');
 // const player = new Vimeo(iframe);
 
-// const onTimeUpdate = function (event) {
-//   localStorage.setItem('videoplayer-current-time', event.seconds);
-// };
+// player.on('timeupdate', throttle(onVideoPlayer, 1000));
 
-// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+// function onVideoPlayer(event) {
+//   let dataString = event.seconds;
+//   localStorage.setItem(STORAGE_KEY, dataString);
+// }
 
-// player.on('timeupdate', onTimeUpdate);
+// const savedSeconds = localStorage.getItem(STORAGE_KEY);
+
+// if (savedSeconds) {
+//   player
+//     .setCurrentTime(savedSeconds)
+//     .then(function (seconds) {})
+//     .catch(function (error) {
+//       switch (error.name) {
+//         case 'RangeError':
+//           break;
+//         default:
+//           break;
+//       }
+//     });
+// }
+
+import Vimeo from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+if (localStorage.getItem('videoplayer-current-time') === undefined) {
+  return;
+} else {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+
+const onTimeUpdate = function (event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+};
+
+player.on('timeupdate', onTimeUpdate);
